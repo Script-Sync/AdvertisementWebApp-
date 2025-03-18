b0d74755019e3a0ae18aab677d7525c392bd6d05
Daniel_mest <danieloppongboahx@gmail.com>
2025-03-18 00:39:52 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+//create an express app
+const app = express();
+
+//Database Connection
+await mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err)
+});
+
+// Use Global middleware
+app.use(cors());
+app.use(express.json());
+
+// Server Listener
+app.listen(3700, () => {
+    console.log("Sever is Running on port 3700");
+});
