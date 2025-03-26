c79b15ee3e0464a6303436cd08e95eb14501b9cd
Daniel_mest <danieloppongboahx@gmail.com>
2025-03-26 16:34:24 +0000

    Fix

--- Routes/adroute.js
+++ Routes/adroute.js
@@ -6,18 +6,18 @@ import {
   getAllAdverts,
   replaceAdvert,
 } from "../Controllers/adController.js";
-import { productPicturesUpload, remoteUpload } from "../Middlewares/upload.js";
+import { productImageUpload, productPicturesUpload, remoteUpload } from "../Middlewares/upload.js";
 import { isAuthenticated, isAuthorized } from "../Middlewares/auth.js";
 
 const advertsRouter = Router();
 
-advertsRouter.post("/ads", isAuthenticated, isAuthorized(['superadmin','admin']), productPicturesUpload.array("images", 2), addAdvert);
+advertsRouter.post("/ads", isAuthenticated, isAuthorized(['superadmin','admin']), productImageUpload.single("image"), addAdvert);
 
 advertsRouter.get("/ads", getAllAdverts);
 
 advertsRouter.get("/ads/:id", getAdvertById);
 
-advertsRouter.put("/:id", isAuthenticated, isAuthorized, productPicturesUpload.array("images", 2), replaceAdvert);
+advertsRouter.put("/:id", isAuthenticated, isAuthorized, productImageUpload.single("image"), replaceAdvert);
 
 advertsRouter.delete("/:id", isAuthenticated,isAuthorized(['admin','superadmin']), deleteAdvert);
 
